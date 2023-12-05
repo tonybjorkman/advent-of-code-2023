d329c44df2b83996fe529a159e956a95a0f6b89f
Tony Björkman <tony_bjorkman@outlook.com>
2023-12-05 16:13:34 +0100

    Solution for part2

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,23 @@
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+index_offset = -1
+def get_cards(card_id):
+    print(f'processing card:{card_id}',end='')
+    if card_id > 202:
+        return 0
+    # return the cards of a single ticket, by index and by reading the value
+    sum=1 #this card
+    for card in [i+card_id for i in range(1,card_score_db[card_id+index_offset]+1) if i+card_id<=202]:
+        sum+=get_cards(card)
+        #print(f'-> Draws card:{card}')
+    
+    #print(f'No more cards to draw for {card_id}. Sum:{sum}')
+    print(f'sum:{sum}')
+    return sum
 
+
+card_score_db = []
 with open('input') as f:
     score = 0
     for line in f.readlines():
@@ -13,6 +29,12 @@ with open('input') as f:
         for value in my_values:
             if value in win_values:
                 count+=1
+        card_score_db.append(count)
         if count>0:
             score+=pow(2,count-1)
     print(f'score is {score}')
+
+sum=0
+for card in range(1,len(card_score_db)+1):
+    sum+=get_cards(card)
+print(f'Solution for Part2:{sum}')
