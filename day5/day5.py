1fc39458945aed25b72632c2f19acace37d4c39b
Tony Björkman <tony_bjorkman@outlook.com>
2023-12-09 18:06:52 +0100

    reworked day5 into recursive solution. part1+2

--- day5/day5.py
+++ day5/day5.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 import os
 from itertools import chain
 import logging
 import tqdm
+import sys
 
 # Set the logging level to DEBUG when you want to enable debug debug_prints
 logging.basicConfig(level=logging.INFO)
@@ -9,88 +11,148 @@ logging.basicConfig(level=logging.INFO)
 def debug_print(message,end='\n'):
     logging.debug(message,end=end)
 
-# Example usage
+class Range:
+    '''
+    Used instead of the inbuilt 'range' since more operations are needed.
+    '''
+    def __init__(self,start,stop) -> None:
+        self.start = start 
+        self.stop = stop 
 
-class Register:
-    def __init__(self, name) -> None:
-        self.name = name
-        self.ranges = []
-        self.linked_register = None
-        self.reverse_link_register = None
+    def add(self,num):
+        self.start+=num
+        self.stop+=num
 
-    def add_link(self, other):
-        self.linked_register = other
-        other.reverse_link_register = self
+    def find_intersection_and_remainder(self,rule):
+        '''
+        range=[1,2]
+        rule=[2,3]
+        range element 2 get intersected
+        '''
+    # Convert range objects to tuples
+        rule_start, rule_end = rule.start, rule.stop
+        self.start, self.stop = self.start, self.stop
 
-    def get_destination(self, source_id):
-        debug_print(f'{self.name}:{source_id} -> ', end='')
-        for dest, source, length in self.ranges:
-            if source_id >= source and source_id < source+length:
-                debug_print(dest+source_id-source)
-                return dest+source_id-source
-        # if not in a range, dest=source
-        debug_print(source_id)
-        return source_id
+        # Check for overlapping conditions
+        if rule_end < self.start or self.stop < rule_start:
+            # Ranges do not overlap
+            return None, Range(self.start,self.stop)
 
-    def get_source(self, dest_id):
-        debug_print(f'{self.name}:{dest_id} -> ', end='')
-        for dest, source, length in self.ranges:
-            if dest_id >= dest and dest_id < dest+length:
-                debug_print(source+dest_id-dest)
-                return source+dest_id-dest
-        # if not in a range, source=dest
-        debug_print(dest_id)
-        return dest_id
+        # Calculate the intersection
+        intersection_start = max(rule_start, self.start)
+        intersection_end = min(rule_end, self.stop)
+        intersection = Range(intersection_start, intersection_end)
 
-    def add_range(self, dest_start, source_start, length):
-        self.ranges.append((dest_start, source_start, length))
+        # Calculate the non-intersecting ranges
+        non_intersecting_ranges = []
+        if self.start < intersection_start:
+            non_intersecting_ranges.append(Range(self.start, intersection_start-1))
+        if intersection_end < self.stop:
+            non_intersecting_ranges.append(Range(intersection_end+1, self.stop))
 
-    def get_final_destination(self, source_id):
-        if self.linked_register is None:
-            return self.get_destination(source_id)
+        return intersection, non_intersecting_ranges
 
-        return self.linked_register.get_final_destination(self.get_destination(source_id))
+    def __repr__(self):
+        return f'Range {self.start}-{self.stop} '
 
-    def get_final_source(self, dest_id):
-        if self.reverse_link_register is None:
-            return self.get_source(dest_id)
+def test_range_intersect():
+    r1 = Range(2,5)  #[2,3,4,5]
+    rule = Range(3,4)  #[3,4]n
 
-        return self.reverse_link_register.get_final_source(self.get_source(dest_id))
+    i,r = r1.find_intersection_and_remainder(rule)
+    print(i)
+    print('remainders')
+    print([x for x in r])
 
+class MapRule(Range):
+    '''
+    stores a range-rule which maps one range to another.
+    start and stop is for the input range, and anything between those
+    will be offset by the offset
+    '''
+    def __init__(self, start, stop,offset) -> None:
+        super().__init__(start, stop)
+        self.offset = offset
+
+    def __repr__(self):
+        return f'RuleRange {self.start}-{self.stop}:{self.offset} '
+
+class Layers:
+    '''
+    Holds all the layers, each layer has a set of mapping rules. 
+    '''
+    def __init__(self) -> None:
+        self.layer_names = []
+        self.layer_rules = [] #each inx is one layer of list 
+
+    def add_layer(self,layer_rules):
+        self.layer_rules.append(layer_rules)
+    
+    def get_rules(self,layer):
+        return self.layer_rules[layer]
+
+class Calculator:
+    '''
+    Performs propagation of the initial seeds through each mapping layer.
+    '''
+    def __init__(self,layers) -> None:
+        self.layers = layers
+        self.min_location = None
+    
+    def propagate(self,in_range:Range,layer):
+        if layer == len(self.layers.layer_rules):
+                if self.min_location is None or in_range.start < self.min_location:
+                    self.min_location = in_range.start
+                return
+        has_intersected = False
+        for rule in self.layers.get_rules(layer): 
+            intersect, remains = in_range.find_intersection_and_remainder(rule)
+            if intersect:
+                has_intersected=True
+                #print(f'layer:{layer} intersect at {intersect.start} -> {intersect.start+rule.offset}')
+                intersect.add(rule.offset)
+                #print(f'inrange:{in_range}')
+                self.propagate(intersect,layer+1)
+                for remain in remains:
+                    self.propagate(remain,layer)
+        # in case none of the rules have applied (thus no other propagate called)
+        if not has_intersected: 
+            self.propagate(in_range,layer+1)
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-registers = []
 seed_ids = []
 seed_ranges = []
+layers = Layers()
 with open('input') as f:
     seedline = f.readline()
     seed_ids = [int(x) for x in seedline.split(":")[1].split()]
 
     for x in range(1, 1+len(seed_ids), 2):
-        seed_ranges.append(range(seed_ids[x-1], seed_ids[x-1] + seed_ids[x]))
-    a = 1
+        seed_ranges.append(Range(seed_ids[x-1], seed_ids[x-1] + seed_ids[x]-1))
+
+    layer_rules=[]
     for line in f.readlines():
         if ':' in line:
-            reg = Register(line[:-2])
-            if len(registers) > 0:
-                registers[-1].add_link(reg)
-            registers.append(reg)
+            layers.layer_names.append(line.split(':')[0])
+            if len(layer_rules)>0:
+                layers.add_layer(layer_rules)
+                layer_rules=[]
         elif len(line) > 1:
             reg_entry = [int(x) for x in line.split()]
-            registers[-1].add_range(*reg_entry)
+            layer_rules.append(MapRule(reg_entry[1],reg_entry[1]+reg_entry[2]-1,reg_entry[0]-reg_entry[1]))
+    
+    if len(layer_rules)>0:
+        layers.add_layer(layer_rules)
 
-def trace_source(location):
-    end_seed = registers[-1].get_final_source(location)
-    print(location)
-    if end_seed in chain(seed_ranges):
-        print(f'Found seed at location {location}')
-        exit()
 
-locations = []
+calc = Calculator(layers)
+
 for seed in seed_ids:
-    debug_print('----')
-    locations.append(registers[0].get_final_destination(seed))
-from joblib import Parallel, delayed
-Parallel(n_jobs=-1)(delayed(trace_source)(location) for location in tqdm.tqdm(range(857589555), desc="Processing", unit="item"))
+    calc.propagate(Range(seed,seed),0)
+print(f'min location part1 {calc.min_location}')
+calc.min_location=None
 
-#print(f'min location: {min(locations)}')
+for seed in seed_ranges:
+    debug_print(f'---- propagating {seed}')
+    calc.propagate(seed,0)
+print(f'min location part2 {calc.min_location}')
