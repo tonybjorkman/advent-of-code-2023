052d023f7f39e64fe2201d1e2d2777a34988a9f2
Tony Björkman <tony_bjorkman@outlook.com>
2023-12-19 14:48:44 +0100

    day11. part1 & 2 finished

--- day11/day11.py
+++ day11/day11.py
@@ -1,38 +1,52 @@
 import numpy as np
 from collections import defaultdict
-galaxies={}
 
-with open('testinput') as f:
-    row = 0
-    col = 0
-    galaxy_number = 1
-    rows = []
-    for line in f:
-        line=line.strip()
-        rows.append([a == '#' for a in line])
-        if not '#' in line:
-            rows.append([False for a in range(len(line))])
-    # list of lines
-    map = np.array(rows)
-    test = np.all(~map,axis=0)
-    indexes = np.nonzero(test)[0]
-    map = np.insert(map,indexes+1,False,axis=1)
+def apply_offsets(col_offsets,row_offsets,position,expansion_factor):
+    col_offsets[col_offsets < position[1]]
+    new_col_inx = position[1]+len(col_offsets[col_offsets < position[1]])*(expansion_factor-1) #-1 because replaces one existing distance
+    new_row_inx = position[0]+len(row_offsets[row_offsets < position[0]])*(expansion_factor-1)
+    return new_row_inx,new_col_inx
 
+def get_galaxy_positions(map,expansion_factor=2):
+    galaxies={}
+    all_false_columns = np.all(~map,axis=0)
+    all_false_rows = np.all(~map,axis=1)
+    col_indexes = np.nonzero(all_false_columns)[0]
+    row_indexes = np.nonzero(all_false_rows)[0]
     galaxies_arr = np.nonzero(map)
+
     galaxies_tuples = zip(galaxies_arr[0],galaxies_arr[1])
     for num,galaxy in enumerate(galaxies_tuples):
-        galaxies[num]=galaxy
-    
-    distance_matrix = np.zeros((len(galaxies),len(galaxies)))
+        galaxies[num]=apply_offsets(col_indexes,row_indexes,galaxy,expansion_factor=expansion_factor)
+    return galaxies
 
+def calculate_distance_matrix(galaxies):
+    distance_matrix = np.zeros((len(galaxies),len(galaxies)))
     for start in range(distance_matrix.shape[0]):
         for dest in range(distance_matrix.shape[0]):
-            if start == dest or dest[1]<start[1]:
+            if start == dest or dest<start:
                 continue
             y_dist=abs(galaxies[start][0]-galaxies[dest][0])
             x_dist=abs(galaxies[start][1]-galaxies[dest][1])
 
             distance_matrix[start,dest] = y_dist+x_dist
-    print(f'total sum {np.sum(distance_matrix)}')
+    return distance_matrix
 
+def get_input_map(file):
+    with open(file) as f:
+        rows = []
+        for line in f:
+            line=line.strip()
+            rows.append([a == '#' for a in line])
+        map = np.array(rows)
+    return map
 
+map = get_input_map('input')
+galaxies = get_galaxy_positions(map,expansion_factor=2)
+exp_2_dist = calculate_distance_matrix(galaxies)
+
+print(f'part1 total sum {np.sum(exp_2_dist)}')
+
+galaxies = get_galaxy_positions(map,expansion_factor=1000000)
+exp_mil_dist = calculate_distance_matrix(galaxies)
+print(f'part2 million total sum {np.sum(exp_mil_dist)}')
