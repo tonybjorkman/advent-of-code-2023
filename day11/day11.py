e8f82e0009ad7b33fdf7a66029d4138302a051c7
Tony Björkman <tony_bjorkman@outlook.com>
2023-12-19 11:43:01 +0100

    day11 part1. numpy in progress

--- day11/day11.py
+++ day11/day11.py
@@ -1,6 +1,6 @@
 import numpy as np
 from collections import defaultdict
-galaxies=defaultdict(list)
+galaxies={}
 
 with open('testinput') as f:
     row = 0
@@ -9,18 +9,30 @@ with open('testinput') as f:
     rows = []
     for line in f:
         line=line.strip()
-        rows.append(list(line))
+        rows.append([a == '#' for a in line])
         if not '#' in line:
-            rows.append(list('.'*(len(line))))
+            rows.append([False for a in range(len(line))])
     # list of lines
-    map = np.array(rows) 
-    a=1
+    map = np.array(rows)
+    test = np.all(~map,axis=0)
+    indexes = np.nonzero(test)[0]
+    map = np.insert(map,indexes+1,False,axis=1)
+
+    galaxies_arr = np.nonzero(map)
+    galaxies_tuples = zip(galaxies_arr[0],galaxies_arr[1])
+    for num,galaxy in enumerate(galaxies_tuples):
+        galaxies[num]=galaxy
     
+    distance_matrix = np.zeros((len(galaxies),len(galaxies)))
 
+    for start in range(distance_matrix.shape[0]):
+        for dest in range(distance_matrix.shape[0]):
+            if start == dest:
+                continue
+            y_dist=abs(galaxies[start][0]-galaxies[dest][0])
+            x_dist=abs(galaxies[start][1]-galaxies[dest][1])
 
-    '''if character == '#':
-        galaxies[(row,col)]=galaxy_number
-        galaxy_number+=1
-       '''         
+            distance_matrix[start,dest] = y_dist+x_dist
+    a=1 
 
 
